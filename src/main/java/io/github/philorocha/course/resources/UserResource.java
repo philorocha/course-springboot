686c677b7ea86ade335d9ae158f34f7e9ca1fccb
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2020-10-07 16:01:56 -0300

    User entity and resource

---
+++ src/main/java/io/github/philorocha/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package io.github.philorocha.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.philorocha.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Felipe", "felipe@gmail.com", "9516-8384", "120859");
+		return ResponseEntity.ok().body(u);
+	}
+}
